e598641b11f486a5643365e19bb6952723e57bd1
ThLegras <38461332+ThLegras@users.noreply.github.com>
2020-02-11 15:35:42 +0100

    ajout des règles

--- lima/transfert.py
+++ lima/transfert.py
@@ -18,12 +18,14 @@ for line in file_ref.readlines():
 	words = line.split()
 	limited_tag.append(words[0])
 	ref_tag.append(words[1])
-	print("0 : "+words[0]+". 1 : "+words[1])
 
 file_ref.close()
 
 for line in file_in.readlines():
 	words = line.split()
+	if(len(words)==0):
+		continue
+	print(words)
 	for i in range(0,len(words) -2):
 		file_out.write(words[i]+" ")
 	if(words[-1]=="SCONJ"):
@@ -38,6 +40,28 @@ for line in file_in.readlines():
 		words[-1]="MD"
 	elif (words[-1]=="PROPN"):
 		words[-1]="NNP"
+	elif(words[-1]=="ADJ"):
+		words[-1]="JJ"
+	elif(words[-1]=="VERB"):
+		words[-1]="VB"
+	elif(words[-1]=="DET"):
+		words[-1]="DT"
+	elif(words[-1]=="ADP"):
+		words[-1]="IN"
+	elif(words[-1]=="NOUN"):
+		words[-1]="NN"
+	elif(words[-1]=="PART"):
+		words[-1]="POS"
+	elif(words[-1]=="CONJ"):
+		words[-1]="CC"
+	elif(words[-1]=="OQU"):
+		words[-1]="."
+	elif(words[-1]=="QUOT"):
+		words[-1]="."
+	elif(words[-1]=="OPAR"):
+		words[-1]="."
+	elif(words[-1]=="CPAR"):
+		words[-1]="."
 	
 	file_out.write("_"+ref_tag[limited_tag.index(words[-1])]+"\n")
 

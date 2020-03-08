8110fa8604404b14ff8892212df6f0ddefb0ff7a
ThLegras <38461332+ThLegras@users.noreply.github.com>
2020-03-08 11:02:44 +0100

    update lima

--- src/exo1.sh
+++ src/exo1.sh
@@ -9,13 +9,24 @@ echo "Lima -> Univ : Done"
 # extract text from univ
 python python/extract_text.py ../data/Exo1/pos_reference.txt.univ ../data/Exo1/pos_test.txt
 echo "Extract text from Univ : Done"
+### lancement des POS taggers ####
 # work on stanford
 cd stanford/stanford-postagger-2018-10-16/
 ./stanford-postagger.sh models/english-left3words-distsim.tagger ../../data/Exo1/pos_test.txt > ../../data/Exo1/pos_test.txt.pos.stanford
 echo "pos_test.txt.pos.stanford : Done"
-cd ..
+# work on lima
+cd ../../../data/Exo1/
+analyzeText -l eng -p main pos_test.txt > pos_test.txt.pos.tmp
+python ../../src/python/formatage_lima.py pos_test.txt.pos.tmp
+rm pos_test.txt.pos.tmp
+cd ../../src
+python python/change_POSTags.py ../data/Exo1/pos_test.txt.pos.lima ../data/Exo1/pos_test.txt.pos.lima.tmp ../data/Exo1/POSTags_LIMA_PTB_Linux.txt
+python python/change_POSTags.py ../data/Exo1/pos_test.txt.pos.lima.tmp ../data/Exo1/pos_test.txt.pos.lima.univ ../data/Exo1/POSTags_PTB_Universal_Linux.txt
+rm ../data/Exo1/pos_test.txt.pos.lima.tmp
 python python/format_postags_stanford.py ../data/Exo1/pos_test.txt.pos.stanford ../data/Exo1/POSTags_PTB_Universal_Linux.txt
 echo "pos_test.txt.pos.stanford.univ : Done"
-# evaluate stanford
+# evaluate
 python python/evaluate.py ../data/Exo1/pos_test.txt.pos.stanford.univ ../data/Exo1/pos_reference.txt.univ
 echo "stanford evaluate : Done"
+python python/evaluate.py ../data/Exo1/pos_test.txt.pos.lima.univ ../data/Exo1/pos_reference.txt.univ
+echo "lima evaluate : Done"
--- src/python/formatage_lima.py
+++ src/python/formatage_lima.py
@@ -7,15 +7,15 @@ if len(sys.argv) != 2:
     raise ValueError("Invalid Arguments : Command line should be 'python formatage_lima.py <input file>")
 
 # Open input and output files
-outputName = sys.argv[1] + ".univ"
+outputName = "pos_test.txt.pos.lima"
 file_in = open(sys.argv[1],"r")
 file_out= open(outputName,"w")
 
 # Tranform to univ
 p = re.compile('\d+ .*\n')
 for line in file_in.readlines():
-	words = line.split()
-	if(len(words)==0):
+	words = line.split('\t')
+	if(len(words)<4):
 		continue
 	if('#' not in words[0]): #si la ligne ne commence pas par un chiffre alors on l'ignore
 		file_out.write(words[2]+"\t"+words[3]+"\n")
